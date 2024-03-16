f06eec7c5f8461b12b61ef0f0b20738dc28cb035
chiyotsai <chiyotsai@gmail.com>
2024-03-15 19:22:05 -0700

    Backing up some files

--- moe_evaluate.py
+++ moe_evaluate.py
@@ -1,16 +1,16 @@
 import argparse
 import os
+import time
 import torch
 import tqdm
-import types
-import numpy as np
 
+from fvcore.nn import FlopCountAnalysis
+from multiprocessing import cpu_count
 from transformers import T5Tokenizer, T5ForConditionalGeneration
-from transformers.models.t5.modeling_t5 import T5DenseActDense
+from torch import profiler
 
 import model_modifier
 from utils import dataset_loader
-from utils import utils
 
 parser = argparse.ArgumentParser()
 
@@ -20,6 +20,8 @@ parser.add_argument('--checkpoint', type=str, default=None, help='Checkpoint to 
 parser.add_argument('--res_path', type=str, default='results/t5-base/base', help='path to store the results of moefication')
 parser.add_argument('--k', type=int, default=20, help='Number of experts to activate')
 parser.add_argument('--eval_type', type=str, choices=['base', 'moe_gt', 'moe_mlp'], help='Number of experts to activate')
+parser.add_argument('--num_batches', type=int, default=None, help='Number of batches used for evaluation')
+parser.add_argument('--batch_size', type=int, default=872, help='Batch size for eval')
 
 
 def main():
@@ -59,33 +61,76 @@ def main():
     output_class_tokens = [class_token.cuda() for class_token in output_class_tokens]
 
     num_correct = 0
-    pbar = tqdm.tqdm(valid)
+    valid_loader = torch.utils.data.DataLoader(
+        valid, batch_size=args.batch_size, shuffle=False,
+    num_workers=max(cpu_count()//2, 1))
+
+    pbar = tqdm.tqdm(valid_loader, total=args.num_batches)
     model.eval()
-    for step, instance in enumerate(pbar):
-        input_ids = tokenizer(text=instance["sentence"], return_tensors="pt").input_ids.cuda()
-        encoder_last_hidden_state = None
-        class_logits = []
-        for class_token in output_class_tokens:
-            if encoder_last_hidden_state is None:
-                output = model(input_ids=input_ids, labels=class_token)
-                encoder_last_hidden_state = output.encoder_last_hidden_state
-            else:
-                output = model(encoder_outputs=(encoder_last_hidden_state,),
-                               labels=class_token)
-            
-            probs = torch.softmax(output.logits, dim=2)
-            logits = torch.log(probs)
-            logits = torch.gather(logits, dim=2, index=class_token[:, :, None]).squeeze(dim=2)
-            class_logits.append(logits.sum(dim=1, keepdim=True))
+    inference_time = 0
+    flops = 0
+    with torch.no_grad():
+        with profiler.profile(activities=[profiler.ProfilerActivity.CPU, profiler.ProfilerActivity.CUDA], record_shapes=True) as prof:
+            for step, instance in enumerate(pbar):
+                if step >= args.num_batches:
+                    break
+                tokenized = tokenizer(text=instance["sentence"], return_tensors="pt", padding=True)
+                input_ids = tokenized.input_ids.cuda()
+                attention_mask = tokenized.attention_mask.cuda()
+                encoder_last_hidden_state = None
+                class_logits = []
+                for class_token in output_class_tokens:
+                    class_token = class_token.repeat((input_ids.size(0), 1))
+                    if encoder_last_hidden_state is None:
+                        start_time = time.time()
+                        with profiler.record_function("model_inference"):
+                            output = model(input_ids=input_ids, attention_mask=attention_mask, labels=class_token)
+                        end_time = time.time()
+                        encoder_last_hidden_state = output.encoder_last_hidden_state
+                    else:
+                        start_time = time.time()
+                        with profiler.record_function("model_inference"):
+                            output = model(encoder_outputs=(encoder_last_hidden_state,),
+                                           attention_mask=attention_mask,
+                                           labels=class_token)
+                        end_time = time.time()
+                    inference_time += end_time - start_time
 
-        class_logits = torch.hstack(class_logits)
-        pred = torch.argmax(class_logits, 1)
+                    if encoder_last_hidden_state is None:
+                        flops += FlopCountAnalysis(model, (input_ids, attention_mask, None, None, None,
+                                                           None, None, None, None, None,
+                                                           None, class_token,)).total()
+                    else:
+                        flops += FlopCountAnalysis(model,
+                                                (None, attention_mask, None, None, None,
+                                                None, None, (encoder_last_hidden_state,), None, None,
+                                                None, class_token,
+                                                )).total()
+                    
+                    probs = torch.softmax(output.logits, dim=2)
+                    logits = torch.log(probs)
+                    logits = torch.gather(logits, dim=2, index=class_token[:, :, None]).squeeze(dim=2)
+                    class_logits.append(logits.sum(dim=1, keepdim=True))
 
-        num_correct += (pred == instance['label']).item()
+                class_logits = torch.hstack(class_logits)
+                pred = torch.argmax(class_logits, 1)
 
-        pbar.set_description(f'Acc: {num_correct/(step + 1):.3f}')
+                num_correct += (pred == instance['label'].cuda()).sum().item()
 
-    print(f'Acc: {num_correct/(step + 1):.3f}')
+                pbar.set_description(f'Acc: {num_correct/((step + 1) * args.batch_size):.3f}')
+
+    accuracy = num_correct / len(valid)
+    inference_time = inference_time / len(valid)
+    flops = flops / len(valid)
+    print(f'Acc: {accuracy:.3f}')
+
+    data_to_save = f"Accuracy: {accuracy}\n\nInference Time: {inference_time}\n\nAverage FLOPs per Example: {flops}\n\nProfiler Summary:\n{prof.key_averages().table(sort_by='cuda_time_total')}\n"
+    save_dir = 'analysis'
+    file_path = f"{save_dir}/{args.model_name}_{args.dataset}_k{args.k}_{args.eval_type}.txt"
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    with open(file_path, "w") as file:
+        file.write(data_to_save)
 
 
 if __name__ == "__main__":
--- preprocess_model.sh
+++ preprocess_model.sh
@@ -9,6 +9,7 @@ K=20
 
 DATASET="sst2"
 RESULT_PATH="results/$MODEL_ARCHITECTURE/$DATASET"
+NUM_EVAL_BATCHES=1
 
 if [[ ! -d "$RESULT_PATH" ]]; then
   mkdir "$RESULT_PATH"
@@ -32,48 +33,50 @@ else
   exit 1
 fi
 
-EVAL_ONLY=0
+EVAL_ONLY=1
 
 echo "Evaluating Baseline Model..."
 cmd=(python moe_evaluate.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
-  --res_path="$RESULT_PATH" --k="$K" --dataset="$DATASET" --eval_type=base)
+  --res_path="$RESULT_PATH" --k="$K" --dataset="$DATASET" --eval_type=base --num_batches="$NUM_EVAL_BATCHES")
 echo "${cmd[*]}"
 ${cmd[@]} | tee -a "$RESULT_PATH/eval_base.log"
 
-if (( ! EVAL_ONLY )); then
-  echo "Clustering..."
-  cmd=(python moe_cluster_experts.py --model_name="$MODEL_ARCHITECTURE" --checkpoint="$MODEL_CHECKPOINT" \
-    --res_path="$RESULT_PATH" --num-expert="$NUM_EXPERTS" --templates="$TEMPLATES")
+if (( 0 )); then
+  if (( ! EVAL_ONLY )); then
+    echo "Clustering..."
+    cmd=(python moe_cluster_experts.py --model_name="$MODEL_ARCHITECTURE" --checkpoint="$MODEL_CHECKPOINT" \
+      --res_path="$RESULT_PATH" --num-expert="$NUM_EXPERTS" --templates="$TEMPLATES")
+    echo "${cmd[*]}"
+    ${cmd[@]}
+  fi
+
+  echo "Evaluating MoE Ground Truth..."
+  cmd=(python moe_evaluate.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
+    --res_path="$RESULT_PATH" --k="$K" --dataset="$DATASET" --eval_type=moe_gt --num_batches="$NUM_EVAL_BATCHES")
   echo "${cmd[*]}"
-  ${cmd[@]}
-fi
+  ${cmd[@]} | tee -a "$RESULT_PATH/eval_moe_gt.log"
 
-echo "Evaluating MoE Ground Truth..."
-cmd=(python moe_evaluate.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
-  --res_path="$RESULT_PATH" --k="$K" --dataset="$DATASET" --eval_type=moe_gt)
-echo "${cmd[*]}"
-${cmd[@]} | tee -a "$RESULT_PATH/eval_moe_gt.log"
+  if (( ! EVAL_ONLY )); then
+    echo "Collecting hidden states..."
+    cmd=(python moe_gather_inf.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
+      --res_path="$RESULT_PATH" --dataset="$DATASET" --num_info_samples="$NUM_MLP_SAMPLES" --shard_file_samples="$MLP_SAMPLE_SHARD_SIZE")
+    echo "${cmd[*]}"
+    ${cmd[@]}
 
-if (( ! EVAL_ONLY )); then
-  echo "Collecting hidden states..."
-  cmd=(python moe_gather_inf.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
-    --res_path="$RESULT_PATH" --dataset="$DATASET" --num_info_samples="$NUM_MLP_SAMPLES" --shard_file_samples="$MLP_SAMPLE_SHARD_SIZE")
+    echo "Training router function..."
+    cmd=(python moe_train_router.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
+      --res_path="$RESULT_PATH" --num-expert="$NUM_EXPERTS" --templates="$TEMPLATES")
+    echo "${cmd[*]}"
+    ${cmd[@]}
+  fi
+  
+  echo "Evaluating MLP..."
+  cmd=(python moe_evaluate.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
+    --res_path="$RESULT_PATH" --k="$K" --dataset="$DATASET" --eval_type=moe_mlp --num_batches="$NUM_EVAL_BATCHES")
   echo "${cmd[*]}"
-  ${cmd[@]}
+  ${cmd[@]} | tee -a "$RESULT_PATH/eval_moe_mlp.log"
 
-  echo "Training router function..."
-  cmd=(python moe_train_router.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
-    --res_path="$RESULT_PATH" --num-expert="$NUM_EXPERTS" --templates="$TEMPLATES")
-  echo "${cmd[*]}"
-  ${cmd[@]}
-fi
- 
-echo "Evaluating MLP..."
-cmd=(python moe_evaluate.py --model_name="$MODEL_ARCHITECTURE"  --checkpoint="$MODEL_CHECKPOINT" \
-  --res_path="$RESULT_PATH" --k="$K" --dataset="$DATASET" --eval_type=moe_mlp)
-echo "${cmd[*]}"
-${cmd[@]} | tee -a "$RESULT_PATH/eval_moe_mlp.log"
-
-if (( ! EVAL_ONLY )); then
-  python t5_train_main.py "--config=configs/moe_t5_base_"$DATASET"_dad_lora_train.yaml"
+  if (( ! EVAL_ONLY )); then
+    python t5_train_main.py "--config=configs/moe_t5_base_"$DATASET"_dad_lora_train.yaml"
+  fi
 fi
